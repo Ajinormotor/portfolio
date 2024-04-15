bd33f7a329a7ee8ad3821babea3a45b3c8f7e64d
Teitei Wisdom <teiteiwisdom@gmail.com>
2024-04-15 07:28:48 +0100

    added social media links

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import "../Footer/Footer.css"
 
@@ -11,10 +12,10 @@ const Footer = () => {
     <h1>Contact Me</h1>
 
     <ul  className="footer__menu">
-    <li><i class="ri-facebook-circle-line"></i></li>
-    <li><i class="ri-linkedin-fill"></i></li>
-    <li><i class="ri-twitter-line"></i></li>
-    <li><i class="ri-github-fill"></i></li>
+    <li><Link to="https://www.facebook.com/profile.php?id=100087303851294"><i class="ri-facebook-circle-line"></i></Link></li>
+    <li><Link to="https://www.linkedin.com/in/teitei-wisdom-1a6965208?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"><i class="ri-linkedin-fill"></i></Link></li>
+    <li><Link to="https://x.com/MachalaFx12?t=njgRcguITkfxU1BxZraayw&s=09"><i class="ri-twitter-line"></i></Link></li>
+    <li><Link to="https://github.com/Ajinormotor"><i class="ri-github-fill"></i></Link></li>
     </ul>
     </div>
 
